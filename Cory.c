55d3ac46be9264f3ed07b8ead3cd491db2023f68
Cory Thrutchley <cory.thrutchley@okstate.edu>
2025-03-31 14:46:26 -0500

    Cory Initial Test
    Created Cory.c and made a hello world style print statement to make sure I can run C code

---
+++ Cory.c
@@ -0,0 +1,11 @@
+// Cory's Week 2 role: Basic IPC Workflow
+// Crease a message queue system where different train processes can send messages to and receive messages from one another
+// Testing: ensure that IPC successfully allows for message passing between trains
+
+#include <stdio.h>
+
+int main() {
+    printf("Initial test\n");
+
+    return 0;
+}

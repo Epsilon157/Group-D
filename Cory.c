9f45718487c5321e442138a8ca32bc9eabc45847
Jabad758 <jabad758@outlook.com>
2025-04-12 09:22:40 -0500

    Update Cory.c
    minor changes for logging

--- Cory.c
+++ Cory.c
@@ -141,6 +141,19 @@ void destroyMessageQueue(int msgid) {
 
 // Function for a train to request to acquire an intersection
 void trainRequestAcquire(int msgid, int trainIndex, const char *intersectionName) {
+
+    log_file = fopen("simulation.log", "a");
+    if (log_file == NULL) {
+        perror("Failed to open simulation.log");
+        exit(EXIT_FAILURE);
+    }
+
+    printRequestSent(trains[trainIndex].name, intersectionName);
+
+    if (log_file) {
+        fclose(log_file);
+    }
+
     Message msg;
     msg.msg_type = 1; // Type 1 = train-to-server
     msg.trainIndex = trainIndex;

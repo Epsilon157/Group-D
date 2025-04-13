63304bca690eaa193ad8d9f4bf74c84ce0f94cfe
Jabad758 <jabad758@outlook.com>
2025-04-13 11:15:43 -0500

    Update shared_header.h

--- shared_header.h
+++ shared_header.h
@@ -28,6 +28,10 @@ typedef struct {
 void printRequestSent(int trainIndex, const char *intersection);
 void printIntersctionGranted(const char *train, const char *intersection);
 void printRequestRelease(int trainIndex, const char *intersection);
+void printIntersectionGranted(const char *trainName, const char *intersectionName);
+
+
+
 int tryAcquireMutex(Intersection *intersection, const char *trainName);
 
 extern int *sim_time;
